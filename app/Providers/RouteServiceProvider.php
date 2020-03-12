0f5fd9bc7eb45e1e251c079cd4cd8449027e5a96
Chinyelu Ibute <chinyeluibute@gmail.com>
2020-03-12 14:41:57 +0100

    changed dashboard to postview and modified  README

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -21,7 +21,8 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    //  public const HOME = '/home';
+     public const HOME = '/postview';
 
     /**
      * Define your route model bindings, pattern filters, etc.
